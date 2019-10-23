10656b0c7e7d907e534fb9cb5edd2e6ebf9be370
Ortal Maslati <ortalmaslati2@gmail.com>
2019-10-23 17:54:54 +0300

    upload first time the app

---
+++ E-Elections-Server/E-Elections-Server/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Elections_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
